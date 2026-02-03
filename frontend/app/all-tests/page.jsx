4416a9455a9de1c4e482256cf1249e9904e550e6
OMKAAR-CELL <omkaarsonamaurya@gmail.com>
2026-02-03 23:55:00 +0530

    created all-test page.

---
+++ frontend/app/all-tests/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '@/Components/Navbar'
+import Footer from '@/Components/Footer'
+
+const page = () => {
+  return (
+    <div>
+        <Navbar />  
+        {/* Page Content */}
+        <Footer />
+    </div>
+  )
+}
+
+export default page
